0163bc0700d7ef71ef40b4a8d88a6ecaf4eb0fc3
Abyan Syauqi <m.abyan.syauqi@gmail.com>
2024-03-27 14:24:10 +0700

    AddScript

---
+++ Assets/AssetAbyan/Script/NPCAI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAI : MonoBehaviour
+{
+    int MovSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("cobak");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
