2c45e5f5f39bb2c37498e6fc18652c6c7ea83b9e
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-04-04 19:39:48 +0700

    Created: Player Movement (Accelerate and Deccelerate)

--- Assets/ScriptsExel/PlayerMovement.cs
+++ Assets/ScriptsExel/PlayerMovement.cs
@@ -2,24 +2,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [Header("Speed & Acceleration")]
+    [SerializeField] float defaultSpeed = 5f;
+    [SerializeField] float accelLimit;
+    
+    float accelStart = 1f;
+    float speed;
     Rigidbody2D rb;
     Vector2 move;
+    bool isSprinting;
+    
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
+    private void Start() {
+        speed = defaultSpeed;
     }
-
     // Update is called once per frame
     void Update()
     {
         move.x = Input.GetAxis("Horizontal");
-        move.y = Input.GetAxis("Vertical");
-        rb.velocity = move * speed;
-
+        if(Input.GetKey(KeyCode.LeftShift)){ // kalau mencet shift, isSprinting set ke true
+            isSprinting = true;
+        }
+        else{ // kalau ga mencet shift, isSprinting set ke false
+            isSprinting = false;
+        }
+        rb.velocity = move * speed * accelStart;
+        if(isSprinting){ // kalau ngeshift. maka accelerate
+            if(accelStart <  accelLimit) accelStart +=  Time.deltaTime;
+        }
+        else{ // kalau ga ngeshift, maka deccelerate
+            if(accelStart > 1) accelStart -= Time.deltaTime;
+        }
     }
 }
