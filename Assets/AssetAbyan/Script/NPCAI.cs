96383c0ca174d93118b920068a2b70a812ec102a
Abyan Syauqi <m.abyan.syauqi@gmail.com>
2024-04-07 16:29:38 +0700

    coin
    ketika dapet coin status berubah ke village dan ketika dalam darius tertentu terdapt coin dia bakal ngejar

--- Assets/AssetAbyan/Script/NPCAI.cs
+++ Assets/AssetAbyan/Script/NPCAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting;
@@ -12,11 +13,17 @@ public class NPCAI : MonoBehaviour
     [SerializeField]Points points;
 
     [SerializeField] bool isIdle;
+    [SerializeField] bool isNight;//Ketika mengejar sesuatu tidak akan berhenti untuk idle(bisa juga kalau pas malam biar NPC balik ke base tanpa ada idle random)
 
     [SerializeField]private int movSpeed;
     [SerializeField]private int direction = 1;
     [SerializeField]private float maksIdelTime;
     [SerializeField]private int idleFrequency;
+
+    [SerializeField]LayerMask coinLayer;
+    [SerializeField]float overLapRadius;
+    [SerializeField]Status status = Status.Poor;
+    
 
     float timer;
     private Rigidbody2D rb;
@@ -24,7 +31,7 @@ public class NPCAI : MonoBehaviour
 
     private void Start()
     {
-        ChangeStatus(Status.Poor);  
+        ChangeStatus();  
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -35,41 +42,46 @@ public class NPCAI : MonoBehaviour
         timer += Time.deltaTime;
         NPCDirection();
         ChangePoint();
+        CheckSurrounding();
     }
     private void FixedUpdate()
     {
         NPCMovement();
     }
-    public void button(){
-        ChangeStatus(Status.Village);
+    private float RandomNumGen(){
+        return UnityEngine.Random.Range(0,5f - maksIdelTime);
     }
-    public void ChangeStatus(Status status)// pada kode ini nantinya akan mengubah points ssuai dengan status dan akan ada kode untuk menyeimbangkan jumlah NPC pada point tertentur
-    {
+    //========================================================================================================================================================
+    public void ChangeStatus()// pada kode ini nantinya akan mengubah points ssuai dengan status dan akan ada kode untuk menyeimbangkan jumlah NPC pada point tertentur
+    {  
         switch (status){
-            case Status.Poor:points = PointManager.instance.getPoint(PointsNames.PoorLeft);
-                currentPoint = points.pointA;
+            case Status.Poor:
+                points = PointManager.instance.getPoint(PointsNames.PoorLeft);
+                SetPoint(points.pointA);
                 break;
             case Status.Village:
-                points =  PointManager.instance.getPoint(PointsNames.PoorRight);
-                currentPoint = points.pointA;
+                points = PointManager.instance.getPoint(PointsNames.Village);
+                SetPoint(points.pointA);
                 break;
         }
     }
     public void ChangePoint(){
         if (currentPoint == points.pointA && math.abs(currentPoint.position.x - rb.position.x) <= 0.5){
-            currentPoint = points.pointB;
+            SetPoint(points.pointB);
             StartCoroutine(NPCIdleForSec());
-            Debug.Log("change");
         }else if(currentPoint == points.pointB && math.abs(currentPoint.position.x - rb.position.x) <= 0.5){
-            currentPoint = points.pointA;
+            SetPoint(points.pointA);
             StartCoroutine(NPCIdleForSec());
-            
         }
     }
+    public void SetPoint(Transform transform){
+        currentPoint = transform;
+    }
+    //============================================================-Movement-============================================================================================
     private void NPCMovement(){
         rb.velocity = new Vector2(movSpeed * direction, rb.velocity.y);
 
-        if (timer >= idleFrequency && rb.velocity.x != 0)
+        if (timer >= idleFrequency)
         {
             Debug.Log("idle");
             StartCoroutine(NPCIdleForSec());
@@ -87,12 +99,34 @@ public class NPCAI : MonoBehaviour
         }
     }
     IEnumerator NPCIdleForSec(){
-        int movSpeed = this.movSpeed;
-        this.movSpeed = 0;
-        yield return new WaitForSeconds(RandomNumGen());
-        this.movSpeed = movSpeed;
+        if (rb.velocity.x != 0 && !isNight)
+        {
+            int movSpeed = this.movSpeed;
+            this.movSpeed = 0;
+            yield return new WaitForSeconds(RandomNumGen());
+            this.movSpeed = movSpeed;
+        }
     }
-    private float RandomNumGen(){
-        return UnityEngine.Random.Range(0,5f - maksIdelTime);
+    //========================================================================================================================================================
+    private void CheckSurrounding(){
+        Collider2D objectInfo = Physics2D.OverlapCircle(transform.position, overLapRadius,coinLayer);
+        if (objectInfo != null && objectInfo.tag == "coin")
+        {
+            SetPoint(objectInfo.transform);
+        }
+    }
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position,overLapRadius);
+    }
+    //========================================================================================================================================================
+    private void OnCollisionEnter2D(Collision2D objectInfo) {
+        if (objectInfo.gameObject.tag == "coin")
+        {
+            Destroy(objectInfo.gameObject);
+            SetPoint(points.pointA);
+            status = Status.Village;
+            ChangeStatus();
+        }
     }
 }
+// masalah : case jika coin diambil NPC lain. Status harusnya diupdate secara berkala (tetapi ada masalah)
