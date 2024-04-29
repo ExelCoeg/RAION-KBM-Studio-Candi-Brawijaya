3d37b4589a0a1f01536573f68225a7bdc7184e2c
Abyan Syauqi <m.abyan.syauqi@gmail.com>
2024-04-29 15:55:56 +0700

    change status and mining

--- Assets/AssetAbyan/Script/NPCAI.cs
+++ Assets/AssetAbyan/Script/NPCAI.cs
@@ -1,81 +1,113 @@
 using System;
 using System.Collections;
+using JetBrains.Annotations;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public enum Status
 {
-    Poor, Villager, Archer
+    Vegrant, Villager, Archer, Knight
 }
 public class NPCAI : MonoBehaviour
 {
-    [SerializeField]Points points;
+    [SerializeField] Points points;
 
     [SerializeField] bool isIdle;
     [SerializeField] bool isNight;//Ketika mengejar sesuatu tidak akan berhenti untuk idle(bisa juga kalau pas malam biar NPC balik ke base tanpa ada idle random)
+    [SerializeField] bool c;
 
     [SerializeField]private int movSpeed;
     [SerializeField]private int direction = 1;
     [SerializeField]private float maksIdleTime;
     [SerializeField]private int idleDelay;
+    [SerializeField] float movementTimer;
+
+
+
+    [SerializeField]private float hp;
+    [SerializeField]private float damage;
+    [SerializeField]private float miningProduct;
+
 
     [SerializeField]LayerMask coinLayer;
     [SerializeField]float overLapRadius;
-    [SerializeField]Status status = Status.Poor;
+    [SerializeField]Status status;
     
 
-    float timer;
+    
     private Rigidbody2D rb;
     private Transform currentPoint;//point that NPC go
 
     private void Start()
     {
-        ChangeStatus();  
+        ChangeStatus(Status.Vegrant);  
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         if (rb.velocity.x == 0){isIdle = true; 
-        }else {isIdle = false;};
-        timer += Time.deltaTime;
+        }else {isIdle = false;}
+
         NPCDirection();
         ChangePoint();
         CheckSurrounding();
+
+        switch (status)
+        {
+            case Status.Vegrant:
+                break;
+            case Status.Villager:
+                minig();
+                break;
+            case Status.Archer:
+                break;
+            case Status.Knight:
+                break;
+            default:
+                break;
+        }
     }
     private void FixedUpdate()
     {
         NPCMovement();
     }
-    private float RandomNumGen(){
-        return UnityEngine.Random.Range(0,5f - maksIdleTime);
+    private float RandomNumGen(float min, float max){
+        return UnityEngine.Random.Range(min,max);
     }
     //========================================================================================================================================================
     // pada kode ini nantinya akan mengubah points ssuai dengan status dan akan ada kode untuk menyeimbangkan jumlah NPC pada point tertentur
 
-    public void ChangeStatus(){  
+    public void ChangeStatus(Status status){  
         switch (status){
-            case Status.Poor:
-                points = PointManager.instance.getPoint(PointsNames.PoorLeft);
+            case Status.Vegrant:
+                points.NPCCount--;
+                this.status = status;
+                points = PointManager.instance.getPoint(PointsNames.LeftVegrant);// nanti rencananya ada pembagian left sama rightnya
                 points.NPCCount++;
-                Debug.Log(points.NPCCount);
                 SetPoint(points.pointA);
                 break;
             case Status.Villager:
+                points.NPCCount--;
+                this.status = status;
                 points = PointManager.instance.getPoint(PointsNames.Villager);
                 points.NPCCount++;
                 SetPoint(points.pointA);
+                break;
+            case Status.Archer:
+                this.status = status;
+                points = PointManager.instance.getPoint(PointsNames.LeftArcher);
+                break;
+            default:
                 break;
         }
     }
     public void ChangePoint(){
         if (currentPoint == points.pointA && math.abs(currentPoint.position.x - rb.position.x) <= 0.5){
             SetPoint(points.pointB);
-            StartCoroutine(NPCIdleForSec());
         }else if(currentPoint == points.pointB && math.abs(currentPoint.position.x - rb.position.x) <= 0.5){
             SetPoint(points.pointA);
-            StartCoroutine(NPCIdleForSec());
         }
     }
     public void SetPoint(Transform transform){
@@ -83,13 +115,14 @@ public class NPCAI : MonoBehaviour
     }
     //============================================================-Movement-============================================================================================
     private void NPCMovement(){
+        movementTimer += Time.deltaTime;
         rb.velocity = new Vector2(movSpeed * direction, rb.velocity.y);
 
-        if (timer >= idleDelay)
+        if (movementTimer >= RandomNumGen(3,5))
         {
             Debug.Log("idle");
-            StartCoroutine(NPCIdleForSec());
-            timer = 0;
+            StartCoroutine(NPCIdleForSec(0.5f,5f));
+            movementTimer = 0;
         }
 
     }
@@ -102,20 +135,46 @@ public class NPCAI : MonoBehaviour
             direction = 1;
         }
     }
-    IEnumerator NPCIdleForSec(){
+    IEnumerator NPCIdleForSec(float min, float max){
         if (rb.velocity.x != 0 && !isNight)
         {
             int movSpeed = this.movSpeed;
             this.movSpeed = 0;
-            yield return new WaitForSeconds(RandomNumGen());
+            yield return new WaitForSeconds(RandomNumGen(min,max));
             this.movSpeed = movSpeed;
         }
     }
+    //===============================================================-HP-=====================================================================================
+    public void setHp(float magnitude){
+        hp += magnitude;
+        if (hp <= 0)
+        {
+            ChangeStatus(Status.Vegrant);
+        }
+    }
+    //===============================================================-Villager-===============================================================================
+    public void minig(){// di update ketika kondisi kita adalah villager
+        //panggil animasi
+        if (isIdle)
+        {
+            miningProduct +=Time.deltaTime;
+            //panggil objek mining untuk menambah point
+            //mengaktifkan mining animation
+        }
+        if (miningProduct >= 10)//waktu untuk menghasilkan 1 koin 
+        {
+            //menambah koin sebanyak 1
+        }
+    }
+    //============================================================-Archer====================================================================================
+
     //========================================================================================================================================================
     private void CheckSurrounding(){
         Collider2D objectInfo = Physics2D.OverlapCircle(transform.position, overLapRadius,coinLayer);
         if (objectInfo != null && objectInfo.tag == "coin")
         {
+            //muncul tombol
+            //tombol bakal ngerubah status is objekInfo
             SetPoint(objectInfo.transform);
         }
     }
@@ -123,15 +182,16 @@ public class NPCAI : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position,overLapRadius);
     }
     //========================================================================================================================================================
-    private void OnTriggerEnter2D(Collider2D objectInfo) {
+    private void OnTriggerEnter2D(Collider2D objectInfo) {// nanti ini ada opsi buat pekerjaannya
         if (objectInfo.tag == "coin")
         {
             Destroy(objectInfo.gameObject);
-            SetPoint(points.pointA);
-            status = Status.Villager;
-            ChangeStatus();
+            ChangeStatus(Status.Villager);
         }
     }
+    //=======================================================-Animation-======================================================================================
+
+
 
 }
 // masalah : case jika coin diambil NPC lain. Status harusnya diupdate secara berkala (tetapi ada masalah)
