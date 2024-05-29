66edd0cce833f34886d53d24ee54c73ca2fd63a1
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-05-29 17:49:23 +0700

    finished UI wireframe

--- Assets/AssetsExel/ScriptsExel/PlayerRecruit.cs
+++ Assets/AssetsExel/ScriptsExel/PlayerRecruit.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class PlayerRecruit : MonoBehaviour
 {
-    public LayerMask npcLayer;
-    public GameObject selectedNPC;
+    [SerializeField] LayerMask npcLayer;
+    private GameObject selectedNPC;
+    public GameObject chooseJobUI;
+    
     private void Update() {
         Collider2D npc = Physics2D.OverlapCircle(transform.position, 1, npcLayer);
-        if(npc != null && Input.GetKeyDown(KeyCode.E)){
+        print(npc != null && npc.gameObject.GetComponent<VegrantScript>());
+        if(npc != null && npc.gameObject.GetComponent<VegrantScript>() && Input.GetKeyDown(KeyCode.E)){
             selectedNPC = npc.gameObject;
+            chooseJobUI.SetActive(true);
         }
-        
-        
-        
     }
 
+    public GameObject getSelectedNPC(){
+        return selectedNPC;
+    }
 }
--- Assets/AssetsExel/ScriptsExel/UI Scripts/Counts.cs
+++ Assets/AssetsExel/ScriptsExel/UI Scripts/Counts.cs
@@ -7,10 +7,14 @@ public class Counts : MonoBehaviour
     public TextMeshProUGUI archerCountText;
     public TextMeshProUGUI villagerCountText;
     
+    public TextMeshProUGUI coinCountText;
     void Update()
     {
         knightCountText.text = NPCManager.instance.knightCount.ToString();
         archerCountText.text = NPCManager.instance.archerCount.ToString();
-        villagerCountText.text =  NPCManager.instance.villagerCount.ToString();
+        villagerCountText.text = NPCManager.instance.villagerCount.ToString();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        coinCountText.text = player.GetComponent<Coin>().coinCount.ToString();
+        
     }
 }
