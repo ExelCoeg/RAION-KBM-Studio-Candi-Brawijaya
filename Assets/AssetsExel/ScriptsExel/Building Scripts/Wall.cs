f2972a0d43c629d20097965079308f07e6c2c54e
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-05-30 18:48:24 +0700

    added: wall

---
+++ Assets/AssetsExel/ScriptsExel/Building Scripts/Wall.cs
@@ -0,0 +1,36 @@
+
+using UnityEngine;
+
+public class Wall : MonoBehaviour, IDamagable
+{
+    public int maxHealth;
+    public int testHealth;
+    public int health {get; set;}
+    public Sprite[] wallStage;
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+    private void Start() {
+        health = maxHealth;
+    }
+    void Update()
+    {
+        health = testHealth;
+        if(health <= 0){
+            GetComponent<SpriteRenderer>().sprite = wallStage[3];
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else if(health <= 20){
+            GetComponent<SpriteRenderer>().sprite = wallStage[2];
+        }
+        else if(health <= 40){
+            GetComponent<SpriteRenderer>().sprite = wallStage[1];
+        }
+        else if(health <= 60){
+            GetComponent<SpriteRenderer>().sprite = wallStage[0];
+        }
+    }
+}
