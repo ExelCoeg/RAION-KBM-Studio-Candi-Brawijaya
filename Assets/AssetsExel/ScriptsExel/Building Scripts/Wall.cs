0b6d4997fb5c363f8e184ea31609420e269d220e
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-05-31 07:15:31 +0700

    added: key icons to walls
    also fixed some bugs

--- Assets/AssetsExel/ScriptsExel/Building Scripts/Wall.cs
+++ Assets/AssetsExel/ScriptsExel/Building Scripts/Wall.cs
@@ -1,4 +1,5 @@
 
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Wall : MonoBehaviour, IDamagable
@@ -7,8 +8,13 @@ public class Wall : MonoBehaviour, IDamagable
     public int testHealth;
     public int health {get; set;}
     public Sprite[] wallStage;
-
-
+    
+    [SerializeField] GameObject EIcon;
+    bool built = false;
+    public void Start()
+    {
+        health = 0;
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -16,20 +22,38 @@ public class Wall : MonoBehaviour, IDamagable
   
     void Update()
     {
-        
-        health = testHealth;
+        Collider2D player = Physics2D.OverlapCircle(new Vector2(transform.position.x,transform.position.y -2f), 3f, LayerMask.GetMask("Player"));
+        if(!built){
+            if(player != null){
+                EIcon.SetActive(true);
+                if(Input.GetKeyDown(KeyCode.F)){
+                    health = maxHealth;
+                    built = true;
+                    GetComponent<BoxCollider2D>().enabled = true;
+                    EIcon.SetActive(false);
+                }
+            }
+            else if(player == null){
+                EIcon.SetActive(false);
+            }
+        }
         if(health <= 0){
             GetComponent<SpriteRenderer>().sprite = wallStage[3];
             GetComponent<BoxCollider2D>().enabled = false;
         }
-        else if(health <= 20){
+        else if(health <= 40){
             GetComponent<SpriteRenderer>().sprite = wallStage[2];
         }
-        else if(health <= 40){
+        else if(health <= 60){
             GetComponent<SpriteRenderer>().sprite = wallStage[1];
         }
-        else if(health <= 60){
+        else if(health <= 100){
             GetComponent<SpriteRenderer>().sprite = wallStage[0];
         }
+    
+    }
+    public void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(new Vector2(transform.position.x,transform.position.y - 2f), 3f);
     }
 }
