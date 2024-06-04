e3a740acd8cf2e9be15876f418d1b796d4c37243
Abyan Syauqi <m.abyan.syauqi@gmail.com>
2024-06-04 12:56:34 +0700

    Info Health

--- Assets/AssetAbyan/Script/NPC/NPCHealth.cs
+++ Assets/AssetAbyan/Script/NPC/NPCHealth.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 public class NPCHealth : MonoBehaviour, IDamagable{
     public int maxHealth = 100;
     public int health {get;set;}
+    public int copyHealth;
     private void Start() {
         health = maxHealth;
     }
     private void Update() {
+
+        copyHealth = health;
         if(health <= 0){
             print("Player dead");
             this.GetComponent<NPC>().ChangeStatus(Status.Vagrant);
