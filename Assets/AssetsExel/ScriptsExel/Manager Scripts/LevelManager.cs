635a9324bf271a4c7427f0d6413512603be61420
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-06-09 08:01:02 +0700

    fix: set npc level texts max

--- Assets/AssetsExel/ScriptsExel/Manager Scripts/LevelManager.cs
+++ Assets/AssetsExel/ScriptsExel/Manager Scripts/LevelManager.cs
@@ -9,6 +9,7 @@ public class LevelManager : MonoBehaviour
     public int knightLevel = 1;
     public int villagerLevel = 1;
 
+    public int maxLevel = 15;
      [SerializeField] TextMeshProUGUI archerLevelText;
      [SerializeField] TextMeshProUGUI knightLevelText;
      [SerializeField] TextMeshProUGUI villagerLevelText;
@@ -19,14 +20,31 @@ public class LevelManager : MonoBehaviour
         else{
             Destroy(gameObject);
         }
-       
-       
-    
     }
     private void Update() {
-        archerLevelText.text = archerLevel.ToString();
-        knightLevelText.text = knightLevel.ToString();
-        villagerLevelText.text = villagerLevel.ToString();
+        if(archerLevel >= maxLevel){
+            archerLevel = maxLevel;
+            archerLevelText.text = "MAX";
+        }
+        else{
+            archerLevelText.text = archerLevel.ToString();
+        }
+        if(knightLevel >= maxLevel){
+            knightLevel = maxLevel;
+            knightLevelText.text = "MAX";
+        }
+        else{
+            knightLevelText.text = knightLevel.ToString();
+        
+        }
+        if(villagerLevel >= maxLevel){
+            villagerLevel = maxLevel;
+            villagerLevelText.text = "MAX";
+        }
+        else{
+
+            villagerLevelText.text = villagerLevel.ToString();
+        }
     }
     
 }
--- Assets/AssetsExel/ScriptsExel/UI Scripts/WorkerStationUI.cs
+++ Assets/AssetsExel/ScriptsExel/UI Scripts/WorkerStationUI.cs
@@ -16,23 +16,25 @@ public class WorkerStationUI : MonoBehaviour
         archerDamageText.text = "Damage " + NPCManager.instance.archerDamage.ToString();
         knightAttackSpeedText.text = "ATKSPD " + NPCManager.instance.knightAttackSpeed.ToString();
         knightDamageText.text = "Damage " + NPCManager.instance.knightDamage.ToString();
-        villagerMiningTimeToCoinText.text = "Mine Time " +NPCManager.instance.villagerMiningTimeToCoin.ToString();
+        villagerMiningTimeToCoinText.text = "Mine Time " +  System.String.Format("{0,0:D2}", NPCManager.instance.villagerMiningTimeToCoin);
     }
     public void ExitUI(){
         gameObject.SetActive(false);
     }
     public void UpgradeKnight(){
+        if(LevelManager.instance.knightLevel >= LevelManager.instance.maxLevel) return;
         NPCManager.instance.knightAttackSpeed += 1.5f;
         NPCManager.instance.knightDamage += 1.5f;
         LevelManager.instance.knightLevel++;
     }
     public void UpgradeVillager(){
-
+        if(LevelManager.instance.villagerLevel >= LevelManager.instance.maxLevel) return;
         NPCManager.instance.villagerMiningTimeToCoin -= 0.2f;
        
         LevelManager.instance.villagerLevel++;
     }
     public void UpgradeArcher(){
+        if(LevelManager.instance.archerLevel >= LevelManager.instance.maxLevel) return;
         NPCManager.instance.archerAttackSpeed += 1.5f;
         NPCManager.instance.archerDamage += 1.5f;
         LevelManager.instance.archerLevel++;
