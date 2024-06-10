63b3ea77fe9ea4d93c9ddc85754c1a1bec10fe26
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-06-11 01:40:36 +0700

    added: hoverOn button

--- Assets/AssetsExel/ScriptsExel/UI Scripts/MainMenuUI.cs
+++ Assets/AssetsExel/ScriptsExel/UI Scripts/MainMenuUI.cs
@@ -1,9 +1,31 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenuUI : MonoBehaviour
-{
+{   
+    public Button startButton;
+    public Button exitButton;
+
+    public Sprite[] startButtonSprites;
+    public Sprite[] exitButtonSprites;
+
+    public void startMouseOn(){
+        startButton.image.sprite = startButtonSprites[0];
+    }
+    public void startMouseOff(){
+        startButton.image.sprite = startButtonSprites[1];
+    }
+    public void exitMouseOn(){
+        exitButton.image.sprite = exitButtonSprites[0];
+    }
+    public void exitMouseOff(){
+        exitButton.image.sprite = exitButtonSprites[1];
+    }
+
+   
     public void StartGame(){
+        
         SceneManager.LoadScene(1);
     }
     public void ExitGame(){
