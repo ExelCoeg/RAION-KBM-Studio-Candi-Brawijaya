8969029d45e094113b3a6ab972972e584b4c1b48
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-06-10 18:54:42 +0700

    finished: petir

--- Assets/AssetsExel/ScriptsExel/Building Scripts/WorkerStation.cs
+++ Assets/AssetsExel/ScriptsExel/Building Scripts/WorkerStation.cs
@@ -1,24 +1,35 @@
 
 using UnityEngine;
 
-public class WorkerStation : MonoBehaviour
+public class WorkerStation : MonoBehaviour, IInteractable
 {
+
+    bool locked = true;
     public int offset = 1;
     [SerializeField] GameObject workerStationUI;
-
+    
     [SerializeField] GameObject NPCCountsUI;
     [SerializeField] GameObject EIcon;
 
     void Update()
     {
-        
-        Collider2D player = Physics2D.OverlapCircle(new Vector2(transform.position.x,transform.position.y - offset), 3f, LayerMask.GetMask("Player"));
+       
+        Collider2D player = Physics2D.OverlapCircle(new Vector2(transform.position.x,transform.position.y - offset), 5f, LayerMask.GetMask("Player"));
         if(player !=null){
-            EIcon.SetActive(true);
-            if(Input.GetKeyDown(KeyCode.F)){
-                workerStationUI.gameObject.SetActive(true);
-                NPCCountsUI.gameObject.SetActive(false);
-                workerStationUI.GetComponent<WorkerStationUI>().closeInput = false;
+                EIcon.SetActive(true);
+            if(!locked){
+                if(Input.GetKeyDown(KeyCode.F)){
+                    workerStationUI.gameObject.SetActive(true);
+                    NPCCountsUI.gameObject.SetActive(false);
+                    workerStationUI.GetComponent<WorkerStationUI>().closeInput = false;
+                }
+            }
+            if(locked){
+                if(Input.GetKeyDown(KeyCode.F)){
+                    EIcon.transform.position = new Vector2(EIcon.transform.position.x, EIcon.transform.position.y - 6);
+                    gameObject.GetComponent<Animator>().Play("workerstation_muncul");
+                    locked= false;
+                }   
             }
         }
 
@@ -29,8 +40,14 @@ public class WorkerStation : MonoBehaviour
         if(!workerStationUI.activeSelf) NPCCountsUI.gameObject.SetActive(true); 
     }
 
+    
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(new Vector2(transform.position.x,transform.position.y - offset), 3f);
+        Gizmos.DrawWireSphere(new Vector2(transform.position.x,transform.position.y - offset), 5f);
+    }
+
+    public void Interact()
+    {
+        
     }
 }
