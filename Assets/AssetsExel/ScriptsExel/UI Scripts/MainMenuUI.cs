a8378d19e60131e79be7721a8d1dfef307e01bbd
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-06-10 14:38:48 +0700

    added: start game and exit game function to buttons

---
+++ Assets/AssetsExel/ScriptsExel/UI Scripts/MainMenuUI.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuUI : MonoBehaviour
+{
+    public void StartGame(){
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame(){
+        Application.Quit();
+    }
+}
