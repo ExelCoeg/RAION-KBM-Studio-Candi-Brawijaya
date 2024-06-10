bfef2be079cb7f811ff63007f630871351818f31
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-06-11 02:29:00 +0700

    fix: building info bug

--- Assets/AssetsExel/ScriptsExel/UI Scripts/PauseUI.cs
+++ Assets/AssetsExel/ScriptsExel/UI Scripts/PauseUI.cs
@@ -1,26 +1,29 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
     bool pause = false;
+
     [SerializeField] GameObject pausePanel;
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.Escape) && !pause){
-            Pause();
-        }
-        if(Input.GetKeyDown(KeyCode.Escape) && pause){
-            Resume();
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(!pause)Pause();
+            else Resume();
         }
 
     }
     public void Pause(){
         pausePanel.SetActive(true);
+        pause = true;
         Time.timeScale = 0;
+        
     }
     public void Resume(){
 
         Time.timeScale = 1;
+        pause = false;
         pausePanel.SetActive(false);
     }
     public void Exit(){
