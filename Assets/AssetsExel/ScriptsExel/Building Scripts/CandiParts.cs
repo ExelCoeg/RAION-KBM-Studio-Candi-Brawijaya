2c240d47fe722668ae23a456526eddcbec51d315
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-06-12 17:53:02 +0700

    fixed: candi animation

--- Assets/AssetsExel/ScriptsExel/Building Scripts/CandiParts.cs
+++ Assets/AssetsExel/ScriptsExel/Building Scripts/CandiParts.cs
@@ -5,25 +5,14 @@ public class CandiParts : MonoBehaviour
 {
     int currentHealth;
     [SerializeField] Sprite[] candiSprites;
-
+    Animator anim;
     // Update is called once per frame
+    private void Awake() {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
         currentHealth = GetComponentInParent<Candi>().currentHealth;
-        if(currentHealth <= 75){
-            GetComponent<SpriteRenderer>().sprite = candiSprites[7];
-        }
-        else if(currentHealth <= 50){
-            GetComponent<SpriteRenderer>().sprite = candiSprites[6];
-        }
-        else if(currentHealth <= 25){
-            GetComponent<SpriteRenderer>().sprite = candiSprites[5];
-        }
-        else if(currentHealth <= 0){
-            GetComponent<SpriteRenderer>().sprite = candiSprites[4];
-            currentHealth = 0;
-            //play animasi ancur then gameover
-        }
-       
+        anim.SetInteger("health",currentHealth);
     }
 }
